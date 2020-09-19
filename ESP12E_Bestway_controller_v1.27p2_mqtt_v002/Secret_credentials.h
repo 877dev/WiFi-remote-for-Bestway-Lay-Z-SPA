0f99a76a93d3d42e73a88aea0fa758324471bd90
877dev <46575282+877dev@users.noreply.github.com>
2020-09-19 08:40:10 +0100

    MQTT_v002 test branch
    Working fine with both web interface and mqtt.

---
+++ ESP12E_Bestway_controller_v1.27p2_mqtt_v002/Secret_credentials.h
@@ -0,0 +1,11 @@
+//ArduinoOTA password
+#define myOTApassword "password"
+
+//MQTT Credentials
+#define myMqttIP (192, 168, 1, 100)
+#define myMqttPort 1883
+#define myMqttUser ""
+#define myMqttPassword ""
+
+
+//See a_globals.h if you wish to customise base topic, client id, ota id, etc...
